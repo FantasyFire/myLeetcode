ae0d8012d59cdb02bfa06a71d7747eb65256da06
FantasyFire <353676041@qq.com>
2020-09-22 11:44:29 +0800

    学习了官方解题思路2，解决本问题

--- 316.去除重复字母.js
+++ 316.去除重复字母.js
@@ -26,31 +26,25 @@
  * 
  */
 /**
+ * https://leetcode-cn.com/problems/remove-duplicate-letters/solution/qu-chu-zhong-fu-zi-mu-by-leetcode/
+ */
+/**
  * @param {string} s
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
-    if (s.length > 1) {
-        var results = [s[0]]
-        for (var i = 1; i < s.length; i++) {
-            var curWord = s[i], pres = s.substr(0, i), idx = pres.indexOf(curWord)
-            if (idx > -1) {
-                // console.log(i, results)
-                var len = results.length
-                for (var j = 0; j < len; j++) {
-                    var ss = results[j]
-                    var ii = ss.indexOf(curWord)
-                    results.push(ss.substr(0, ii) + ss.substring(ii + 1) + curWord)
-                }
-                // console.log(i, results)
-            } else {
-                results = results.map(r => r + curWord)
-            }
-        }
-        results.sort()
-        // console.log(results)
-    }
+    var r = '', lasti = {}
+    for (var j = 0; j < s.length; j++) lasti[s[j]] = j // 记录每个字母最后出现的位置，方便后面查找后方是否有重复的字母
+    for (var i = 0; i < s.length; i++) { // O(n)
+        var w = s[i], wi = r.indexOf(w)
+        if (wi > -1) { // 由于else的操作，可以确保有重复字母的情况下，前面的字符串字典序一定比后面高，所以这里直接舍弃后面的字符
 
-    return s.length > 1 ? results[0] : s
+        } else { // 结果中尚无此字符，插入前，先将栈顶的字典序比当前字符小，且后方有重复的字符去除，以确保r中的字典序最高
+            while (r && r[r.length-1] > w && lasti[r[r.length-1]] > i) r = r.substr(0, r.length-1)
+            r += w
+        }
+        // console.log(i, r)
+    }
+    return r
 };
 
