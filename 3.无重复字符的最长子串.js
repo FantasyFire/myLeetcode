8c458598993b2e560aae387fdd0f50ecf14ddfae
FantasyFire <353676041@qq.com>
2020-09-22 11:59:21 +0800

    解决无重复最长子串问题

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -10,7 +10,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-
+    var max_len = 0, cur_s = '', s_len = s.length
+    for (var i = 0; i < s_len; i++) {
+        var w = s[i], wi = cur_s.indexOf(w)
+        if (wi > -1) {
+            max_len = Math.max(max_len, cur_s.length)
+            cur_s = cur_s.substring(wi + 1) + w
+        } else {
+            cur_s += w
+        }
+    }
+    return Math.max(max_len, cur_s.length)
 };
 // @lc code=end
 
