79997e40213b0676c2083307bd62ff4d90f6e650
FantasyFire <353676041@qq.com>
2020-09-22 17:04:16 +0800

    用官方动态规划思路，结果没原来的好

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -9,7 +9,42 @@
  * @param {string} s
  * @return {string}
  */
+/**
+ * 103/103 cases passed (728 ms)
+ * Your runtime beats 15.6 % of javascript submissions
+ * Your memory usage beats 9.57 % of javascript submissions (65.2 MB)
+ */
+var longestPalindrome = function(s) {
+    var palindrome = [], len = s.length, max = -1, ri = 0, rj = 0
+    // 初始化n*n二维数组，表示从第i到j的子串是否回文
+    for (var i = 0; i < len; i++) palindrome[i] = (new Array(len)).fill(false)
+    // 遍历数组
+    for (var l = 0; l < len; l++) { // 子串长度
+        for (var i = 0; i < len; i++) { // 子串起始点i
+            var j = i + l // 子串末点j
+            if (j >= len) break
+            if (l == 0) palindrome[i][j] = true // 显然单个字母是回文
+            if (l == 1) palindrome[i][j] = s[i] == s[j] // 长度为2的子串
+            if (l > 1) palindrome[i][j] = palindrome[i+1][j-1] && s[i] == s[j] // 其他情况
+            if (palindrome[i][j] && l > max) {
+                max = l
+                ri = i
+                rj = j + 1
+            }
+            // console.log(i, j, max, ri, rj)
+        }
+    }
+    // console.log(palindrome)
+    return s.substring(ri, rj)
+};
+
+
 // 自己想的暴力解法
+/**
+ * 103/103 cases passed (144 ms)
+ * Your runtime beats 66.67 % of javascript submissions
+ * Your memory usage beats 79.97 % of javascript submissions (38.3 MB)
+ */
 // var longestPalindrome = function(s) {
 //     // var palindrome = {}
 //     var len = s.length, max = 0, r = ''
