6b5ea0a957b9a2c6dc146a5527c709647b480422
FantasyFire <353676041@qq.com>
2020-09-23 18:24:29 +0800

    上传

---
+++ 93.复原ip地址.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=93 lang=javascript
+ *
+ * [93] 复原IP地址
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+/**
+ * 自己想的动规方法
+ */
+var restoreIpAddresses = function(s) {
+    return dp()
+};
+
+var dp = function (s, si, deep, cache) {
+    
+}
+// @lc code=end
+
