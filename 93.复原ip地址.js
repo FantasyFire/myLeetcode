f2a933bad411a620e6cb270cc0058939b4da1aed
FantasyFire <353676041@qq.com>
2020-09-25 16:37:05 +0800

    93.复原ip地址，自己的动规方法提交成功

--- 93.复原ip地址.js
+++ 93.复原ip地址.js
@@ -11,13 +11,48 @@
  */
 /**
  * 自己想的动规方法
+ * 147/147 cases passed (92 ms)
+ * Your runtime beats 53.68 % of javascript submissions
+ * Your memory usage beats 22.37 % of javascript submissions (38.8 MB)
  */
 var restoreIpAddresses = function(s) {
-    return dp()
+    var cache = [[],[],[],[]]
+    var r = dp(s, 0, 3, cache)
+    return r
 };
 
 var dp = function (s, si, deep, cache) {
-    
+    var r = []
+    if (!cache[deep][si]) {
+        if (deep) {
+            if (s[si]) {
+                r = r.concat(dp(s, si + 1, deep - 1, cache).map(c => `${s[si]}.${c}`))
+
+                // 排除前导0
+                if (s[si] != '0') {
+                    if (s.length > si + 2) {
+                        var n = s.substr(si, 3) * 1
+                        if (n <= 255)
+                            r = r.concat(dp(s, si + 3, deep - 1, cache).map(c => `${n}.${c}`))
+                    }
+                    if (s.length > si + 1) r = r.concat(dp(s, si + 2, deep - 1, cache).map(c => `${s[si]}${s[si+1]}.${c}`))
+                }
+            }
+        } else {
+            if (s[si]) {
+                var remain = s.substring(si)
+                if (si == s.length - 1) {
+                    r.push(s[si])
+                } else if (remain * 1 <= 255 && s[si] != '0') {
+                    r.push(remain)
+                }
+            }
+        }
+        cache[deep][si] = r
+    } else {
+        r = cache[deep][si]
+    }
+    return r
 }
 // @lc code=end
 
